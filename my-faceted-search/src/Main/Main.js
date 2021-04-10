2108f7456321e29f981d233c6ebe1acc80619018
Doctor <mr.seress@Mrs-MacBook-Pro.local>
2021-04-10 20:21:28 +0200

    Implement search feature

--- my-faceted-search/src/Main/Main.js
+++ my-faceted-search/src/Main/Main.js
@@ -4,10 +4,19 @@ class Main extends Component {
   constructor() {
     super();
     this.state = {
-      filters: [],
+      terms: [],
+      filters: [{
+        'title': 'All',
+        'node': [{
+          'id': 'firstfilter',
+          'checked': false,
+          'name': 'all',
+          'status': 'enabled'
+        }]
+      }],
       cards: [],
       urls: [],
-      hasData: true,
+      searchtext: '',
     }
 
     this.getUrls = this.getUrls.bind(this);
@@ -15,11 +24,14 @@ class Main extends Component {
 
 
   getUrls = (url, currentUrl = url) => {
+
     Promise.all([
       fetch(`${url}`, {'method': 'GET'}),
     ])
       .then(values => Promise.all(values.map(value => value.json())))
       .then(data=>{
+        const terms = [...this.state.terms];
+
         if (url === currentUrl) {
           let cards = [...this.state.cards];
 
@@ -34,6 +46,17 @@ class Main extends Component {
                     imageurl: "https://devportalawards.org/" + data[0]['included'][index]['attributes']['uri']['url'],
                     title: element['attributes']['title'].toString(),
                     sitelink: element['attributes']['field_sitelink'][0]['uri'],
+                    category: element['relationships']['field_categories']['data'].map(e => {
+                      let name;
+                      terms.map(category => {
+                        console.log(category)
+                        if (e.id === category.id) {
+                        name = category.name;
+                        }
+                      })
+      
+                      return name;
+                    }),
                   }
                 ]
               ]
@@ -71,36 +94,66 @@ class Main extends Component {
       });
   }
 
+  inputHandler = (event) => {
+    this.setState({searchtext: event.target.value})
+  }
+
+  submitHandler = (event) => {
+    event.preventDefault();
+    const searchText = this.state.searchtext
+    let cards = [];
+
+    if (searchText === '') {
+      return
+    }
+
+    this.setState({urls:[]});
+
+    Promise.all([
+      fetch('https://devportalawards.org/jsonapi/node/nominees?filter[field_ongoing][value]=1&include=field_site_image&fields[file--file]=uri', {'method': 'GET'}),
+    ])
+      .then (values => Promise.all(values.map(value => value.json())))
+      .then(data => {
+        data[0]['data'].map((element, index) => {
+          cards = [
+            ...cards,
+            ...[{
+              imageurl: "https://devportalawards.org/" + data[0]['included'][index]['attributes']['uri']['url'],
+              title: element['attributes']['title'].toString(),
+              sitelink: element['attributes']['field_sitelink'][0]['uri'],
+            }]
+          ]
+        })
+
+        cards = cards.filter(card => card.title.toLowerCase().includes(searchText.toLowerCase()))
+
+        this.setState({cards: cards});
+      })
+  }
+
   handleCheckChieldElement = (event) => {
     this.setState({cards: []});
     this.setState({urls: []})
-    this.setState({hasData: true});
-
-    console.log(event)
-
-    const filters = [...this.state.filters]
-    filters.forEach(element => {
-      element.node.map(e=>{
-        if (e.name === event.target.value) {
-          e.isChecked =  event.target.checked
-        }
-          
-      return filters
-      })
-    })
-
+    const filters = [...this.state.filters];
 
     filters.map(filter => {
       filter.node.map(e => {
-        if (e.isChecked === true) {
-          this.getUrls(`https://devportalawards.org/jsonapi/node/nominees?filter[field_categories.drupal_internal__tid]=${e.id}&filter[field_ongoing][value]=1&page[limit]=5&include=field_site_image&fields[file--file]=uri`);
+        if (event.target.id == e.id) {
+          e.checked = true;
+        } else {
+          e.checked = false;
         }
       })
     })
 
-    if (!filters.some(e => e.node.some(el=> el.isChecked=== true))) {
+    this.setState({filters:filters})
+
+    if (event.target.id === 'firstfilter') {
       this.getUrls('https://devportalawards.org/jsonapi/node/nominees?filter[field_ongoing][value]=1&page[limit]=5&include=field_site_image&fields[file--file]=uri');
     }
+
+    this.getUrls(`https://devportalawards.org/jsonapi/node/nominees?filter[field_categories.drupal_internal__tid]=${event.target.id}&filter[field_ongoing][value]=1&page[limit]=5&include=field_site_image&fields[file--file]=uri`);
+
   }
 
   handlePaginaTion = (e) => {
@@ -139,17 +192,15 @@ class Main extends Component {
 
   componentDidMount() {
     const urls =['https://devportalawards.org/jsonapi/node/nominees?filter[field_ongoing][value]=1&page[limit]=5&include=field_site_image&fields[file--file]=uri'];
-    this.getUrls(urls[0]);
-
+    const terms = [];
     
-
     Promise.all([
       fetch('https://devportalawards.org/jsonapi/taxonomy_term/category', {'method': 'GET'}),
       fetch('https://devportalawards.org/jsonapi/taxonomy_term/category_group', {'method': 'GET'}),
     ])
       .then (values => Promise.all(values.map(value => value.json())))
       .then(data => {
-        let categoryGroup = [];
+        let categoryGroup = [...this.state.filters];
 
         data[1]['data'].map(element => {
           categoryGroup = [
@@ -163,60 +214,65 @@ class Main extends Component {
           }]];
         });
 
-        categoryGroup.map(el => {
-          el.relatedCategories.map(e => {
-            data[0]['data'].map((data, i)=> {
-              if (e === data['id']) {
-                 el.node.push({
-                   name: data['attributes']['name'],
-                   isChecked: false,
-                   id: data['attributes']['drupal_internal__tid'],
-                   status: 'enabled',
-                 })
-              }
-            })
+        this.setState({terms: 
+          data[0]['data'].map((data, i)=> {
+            return {
+              name: data['attributes']['name'],
+              id: data['id'],
+            }    
           })
+        })
 
-          el.node.map(filter=> {
-            Promise.all([
-              fetch(`https://devportalawards.org/jsonapi/node/nominees?filter[field_categories.drupal_internal__tid]=${filter.id}&filter[field_ongoing][value]=1&page[limit]=1`, {'method': 'GET'}),
-            ])
-            .then (values => Promise.all(values.map(value => value.json())))
-            .then(data => {
-              if (data[0]['data'].length < 1) {
-                filter.status = 'disabled';
-              }
-              
-              this.setState({filters:categoryGroup})
+        categoryGroup.map((el, index) => {
+          if (index > 0) {
+            el.relatedCategories.map(e => {
+              data[0]['data'].map((data, i)=> {
+                if (e === data['id']) {
+                   el.node.push({
+                     name: data['attributes']['name'],
+                     id: data['attributes']['drupal_internal__tid'],
+                     checked: false,
+                   })
+                }
+              })
             })
-          })
-        })   
+          }
+        })
+
+        this.setState({filters:categoryGroup})
+        this.getUrls(urls[0]);
+
       })
   };
 
   render() {
     return (
       <div>
+        <form action="search">
+            <input type="text" id="search" name="search" value={this.state.searchtext} onChange={(e)=>this.inputHandler(e)}/>
+             <input type="submit" value="Submit" onClick={(e)=>this.submitHandler(e)}/>
+             
+         </form> 
         <div className='filters'>
-          {this.state.filters.map(e=> {
+          {this.state.filters.map((e, index)=> {
             return <div key={e.title}>
               <h3>{e.title}</h3>
               {e.node.map(el => {
                 return <div key={el.id}>
-                  <input id={el.id} type="checkbox" value={el.name} status={el.status} onChange={this.handleCheckChieldElement}/>
+                  <input id={el.id} type="radio" value={el.name} checked={el.checked} name={el.name} onChange={this.handleCheckChieldElement}/>
                   <label>{el.name}</label>
                   </div>
               })}
             </div>
             })}
         </div>
-        {this.state.hasData ? 
+        {this.state.cards.length > 0 ? 
         this.state.cards.map(card=> {
           return <div key={card.title}>
             <h2>{card.title}</h2>
             <img src={card.imageurl}/>
           </div>
-        }) : <div>Lofasztsetalaltal</div>}
+        }) : <h1>Lofasztsetalaltal</h1>}
   
         <ul>
           {this.state.urls.map((url, index)=>{
